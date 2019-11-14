f374efd70b7dc85d4c919646aabd7c3d0c35c7e2
Duong <ngy.duong@gmail.com>
2019-11-14 17:22:00 +0100

    test

--- titanic_script.py
+++ titanic_script.py
@@ -1,7 +1,6 @@
 # ==================== PACKAGES ==================== #
 
 import pandas as pd
-# import os
 import numpy as np
 import sklearn
 
@@ -16,7 +15,7 @@ train = pd.read_csv("titanic_data/train.csv")
 cabin_list = ['A', 'B', 'C', 'D', 'E', 'F', 'T', 'G', 'Unknown']
 train['Deck']=train['Cabin'].map(lambda x: substrings_in_string(x, cabin_list))
 
-
+xx
 
 
 
