b6a82a170fccc9047a5ac441b2223e9a5c73e6b3
Duong <ngy.duong@gmail.com>
2019-11-14 18:30:13 +0100

    clean data

--- titanic_script.py
+++ titanic_script.py
@@ -1,9 +1,10 @@
 # ==================== PACKAGES ==================== #
 
 import pandas as pd
-# import os
 import numpy as np
 import sklearn
+import re
+import math
 
 # ==================== IMPORT DATA ==================== #
 
@@ -12,15 +13,22 @@ train = pd.read_csv("titanic_data/train.csv")
 
 # ==================== CLEANING DATA ==================== #
 
-#Turning cabin number into Deck
-cabin_list = ['A', 'B', 'C', 'D', 'E', 'F', 'T', 'G', 'Unknown']
-train['Deck']=train['Cabin'].map(lambda x: substrings_in_string(x, cabin_list))
+##  We create the variable Famsize as family size, the size of each family on board
+train['Famsize'] = train['SibSp'] + train['Parch']
 
+# # We convert all observations from Cabin variable into string
+# train["Cabin"] = train["Cabin"].apply(lambda x: str(x))
 
+#  We extract the Deck from cabin number and name the new variable Deck
 
+def get_deck(x):
+    if pd.isnull(x) == True :
+        return(x)
+    else:
+        str_x = str(x)
+        deck = re.sub('[^a-zA-Z]+', '', str_x)
+        Deck = "".join(sorted(set(deck), key=deck.index))
+        return(Deck.upper())
 
-
-
-
-
+train["Deck"] = train["Cabin"].apply(lambda x: get_deck(x))
 
