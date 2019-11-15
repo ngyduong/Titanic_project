fb93aecc976c42b9d5c9bcb5ed77bdc864a9e9e8
Duong <ngy.duong@gmail.com>
2019-11-15 09:04:21 +0100

    data cleaning

--- titanic_script.py
+++ titanic_script.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 import sklearn
 import re
-import math
 
 # ==================== IMPORT DATA ==================== #
 
@@ -13,22 +12,34 @@ train = pd.read_csv("titanic_data/train.csv")
 
 # ==================== CLEANING DATA ==================== #
 
-##  We create the variable Famsize as family size, the size of each family on board
+# //-- We create the variable Famsize as family size, the size of each family on board \\-- #
+
 train['Famsize'] = train['SibSp'] + train['Parch']
+test['Famsize'] = test['SibSp'] + test['Parch']
 
-# # We convert all observations from Cabin variable into string
-# train["Cabin"] = train["Cabin"].apply(lambda x: str(x))
-
-#  We extract the Deck from cabin number and name the new variable Deck
+# //-- We extract the Deck from cabin number and name the new variable Deck \\-- #
 
 def get_deck(x):
     if pd.isnull(x) == True :
         return(x)
     else:
-        str_x = str(x)
-        deck = re.sub('[^a-zA-Z]+', '', str_x)
+        deck = re.sub('[^a-zA-Z]+', '', x)
         Deck = "".join(sorted(set(deck), key=deck.index))
         return(Deck.upper())
 
+# This function check if the value is null, if it's true then the function return the null value
+# If it's false then the function extract only unique characters from strings and return the upper case value
+
 train["Deck"] = train["Cabin"].apply(lambda x: get_deck(x))
+test["Deck"] = test["Cabin"].apply(lambda x: get_deck(x))
+
+# //-- We check the missing values in each dataset \\-- #
+
+train.isnull().sum()
+test.isnull().sum()
+
+# It seems that there is mostly missing values for the variables Cabin, Embarked, age and Deck
+
+
+
 
