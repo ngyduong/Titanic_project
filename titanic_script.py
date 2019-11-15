6feeb6650d9b8393cf0fb8ac82712f1d5b82ae42
Duong <ngy.duong@gmail.com>
2019-11-15 10:13:49 +0100

    merge data and fill age missing value

--- titanic_script.py
+++ titanic_script.py
@@ -2,20 +2,33 @@
 
 import pandas as pd
 import numpy as np
-import sklearn
 import re
+import matplotlib as plt
+import seaborn as sns
+import sklearn
+from sklearn.preprocessing import Imputer
 
 # ==================== IMPORT DATA ==================== #
 
 test = pd.read_csv("titanic_data/test.csv")
 train = pd.read_csv("titanic_data/train.csv")
 
+# save PassengerId for final submission
+passengerId = test.PassengerId
+
+# create dummy variables to separate data later on
+# if train = 1 the observation was in the train originally
+train["train"] = 1
+test["train"] = 0
+
+# merge train and test
+titanic = train.append(test, ignore_index=True)
+
 # ==================== CLEANING DATA ==================== #
 
 # //-- We create the variable Famsize as family size, the size of each family on board \\-- #
 
-train['Famsize'] = train['SibSp'] + train['Parch']
-test['Famsize'] = test['SibSp'] + test['Parch']
+titanic['Famsize'] = titanic['SibSp'] + titanic['Parch']
 
 # //-- We extract the Deck from cabin number and name the new variable Deck \\-- #
 
@@ -26,19 +39,37 @@ def get_deck(x):
         deck = re.sub('[^a-zA-Z]+', '', x)
         Deck = "".join(sorted(set(deck), key=deck.index))
         return(Deck.upper())
-
 # This function check if the value is null, if it's true then the function return the null value
 # If it's false then the function extract only unique characters from strings and return the upper case value
 
-train["Deck"] = train["Cabin"].apply(lambda x: get_deck(x))
-test["Deck"] = test["Cabin"].apply(lambda x: get_deck(x))
+titanic["Deck"] = titanic["Cabin"].apply(lambda x: get_deck(x))
 
 # //-- We check the missing values in each dataset \\-- #
 
-train.isnull().sum()
-test.isnull().sum()
+titanic.isnull().sum()
+# It seems that there is mostly missing values for the variables Cabin, Embarked, age, Fare and Deck
+# There is 418 missing value for survived because in the original test data set there was no variable survived
 
-# It seems that there is mostly missing values for the variables Cabin, Embarked, age and Deck
+# //-- Let's try now to fill the missing values for each variables \\-- #
+
+## We begin with age ##
+
+# Let's have a first statistical analysis
+
+titanic.Age.describe()
+titanic.Age.hist()
+# The mean and the median are close to each other with 29.7 and 28 respectively
+# Most of the observations are between 20 and 30 yo
+# We can maybe replace the missing values by the median
+
+# We now will replace the missing age by it's median
+
+imp = Imputer(missing_values='NaN', strategy='median', axis=1)
+titanic["New_age"] = imp.fit_transform(titanic['Age'].values.reshape(1, -1)).T
+# We now have new age as our new age variable without missing values
+
+
+
 
 
 
