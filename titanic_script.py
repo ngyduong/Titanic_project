5979da80773c65ca41a12d59c763f6afbf769a58
Duong <ngy.duong@gmail.com>
2019-11-16 21:44:14 +0100

    Remove cabin and deck + data cleaning + creating derived variables

--- titanic_script.py
+++ titanic_script.py
@@ -1,13 +1,15 @@
 # ==================== PACKAGES ==================== #
 
 import pandas as pd
-import numpy as np
-import re
-import matplotlib as plt
-import seaborn as sns
-import sklearn
+# import numpy as np
+# import re
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import sklearn
+
 
 # ==================== DATA MANIPULATION ==================== #
+
 
 test = pd.read_csv("titanic_data/test.csv")
 train = pd.read_csv("titanic_data/train.csv")
@@ -20,27 +22,9 @@ test["Train_set"] = 0
 # merge train and test
 titanic = train.append(test, ignore_index=True, sort=False)
 
+
 # ==================== CLEANING DATA ==================== #
 
-# //-- New variable Famsize \\-- #
-
-titanic['Famsize'] = titanic['SibSp'] + titanic['Parch'] + 1
-# We create the variable Famsize as family size, the size of each family on board
-# It is important to add +1 because we have to count the person itself as member of the family
-
-# //-- We extract the Deck from cabin number and name the new variable Deck \\-- #
-
-def get_deck(x):
-    if pd.isnull(x) == True :
-        return(x)
-    else:
-        deck = re.sub('[^a-zA-Z]+', '', x)
-        Deck = "".join(sorted(set(deck), key=deck.index))
-        return(Deck.upper())
-# This function check if the value is null, if it's true then the function return the null value
-# If it's false then the function extract only unique characters from strings and return the upper case value
-
-titanic["Deck"] = titanic["Cabin"].apply(lambda x: get_deck(x))
 
 # //-- We check the missing values in each dataset \\-- #
 
@@ -112,25 +96,56 @@ titanic.Embarked.fillna("S", inplace=True)
 
 ## Fill NaN in Fare variable ##
 
-titanic.Fare.describe()
-titanic.Fare.hist()
-# The mean Fare is 33.3 meanwhile the median is 14.45
-# Most of the distribution is 0 and 50, let's plot the distribution of Fare only between 0 and 50
+Fare_by_title = pd.DataFrame({'mean_fare': titanic.groupby('Title').mean().loc[:, "Fare"],
+                              'median_fare': titanic.groupby('Title').median().loc[:, "Fare"],
+                              'count': titanic.Title.value_counts(),
+                              'fare_missing': titanic.Fare.isnull().groupby(titanic['Title']).sum()})
 
-fare_filtered = pd.Series(filter(lambda x: x <= 0, titanic.Fare))
-fare_filtered.hist()
-# Most of the distribution is actually before 15 so I have decided to replace the missing value by it's median
+# As there is only 1 missing value from Fare, and by looking at this dataframe
+# we can see that the only missing Fare is from a passenger with the title "Mr"
+# I will therefore replace the missing value with the median value of Fare for "Mr"
 
-# let's replace the missing fare it's median
-titanic.Fare.fillna(titanic.Fare.median(), inplace=True)
+titanic.Fare.fillna(Fare_by_title.loc['Mr', "median_fare"], inplace=True)
 
-## Fill NaN in Deck variable ##
+## Dropping the variable Cabin ##
 
-# As there is too many missing values for Deck (and henceforh Cabin) with over 1000 missing values
-# it is not wise to replace the value by the most common value or drop all observations with missing values
-# Therefore i will replace the missing values by a "Unknown" for unknown deck
+# As there is too many missing value for Cabin, 1014 missing values over 1309 it's not wise to keep the
+# variable as it can create noises in the prediction. Therefore I have decided to remove the variable from the dataset
+titanic = titanic.drop(columns="Cabin")
 
-titanic.Deck.fillna("Unknown", inplace=True)
+
+# ==================== Creating derived variables ==================== #
+
+
+# //-- Derived variables from SibSp and Parch \\-- #
+
+## We create the variable Famsize as family size, the size of each family on board
+titanic.Famsize = titanic['SibSp'] + titanic['Parch'] + 1
+# It is important to add +1 because we have to count the person itself as member of the family
+
+titanic.Famsize.value_counts()
+titanic.Famsize.hist()
+# Given the distribution of the variable we can create categorical variables based on group of family size
+
+# I will therefore replace Famsize by it's categorical variable as :
+# Famsize = solo if the person has no family on board and is travelling alone
+# Famsize = small_family if the person has 2 or 3 members of his family on board (parents/children/siblings/spouses)
+# Famsize = big_family if the person has strictly more than 4 members on his family on board
+
+def Famsize_categorical(x):
+    if x == 1:
+        return("solo")
+    if x in [2,3]:
+        return ("small_family")
+    elif x > 3:
+        return("big_family")
+    else:
+        return("ERROR")
+
+titanic["Famsize"] = titanic.Famsize.apply(lambda x: Famsize_categorical(x))
+
+# //-- Derived variables from Age \\-- #
+
 
 
 
