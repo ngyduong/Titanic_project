dfb3b7c70ea67b280952bc7f4e5e6f896ce9279f
Duong <ngy.duong@gmail.com>
2019-11-21 16:50:13 +0100

    data cleaning

--- data_cleaning.py
+++ data_cleaning.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sklearn
 
+from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score, RandomizedSearchCV
 
@@ -65,7 +66,6 @@ titanic["Title"] = titanic.Title.apply(lambda x: generalized_title(x))
 
 # As we need the data in categorical form we will create dummy variables from Title
 titanic = pd.concat([titanic, pd.get_dummies(titanic["Title"])], axis=1)
-titanic=titanic.drop('Title', axis=1)
 
 # //-- CREATES FAMSIZE \\-- #
 
@@ -135,45 +135,56 @@ titanic.Fare.fillna(titanic.Fare.median(), inplace=True)
 # As there is only 1 missing value for Fare we can replace the missing value by it's median
 
 
-# ==================== Fill NaN in Age variable with RANDOM FOREST ==================== #
-
+# ==================== Fill NaN in Age variable  ==================== #
 
 # Let's have a first statistical analysis
-
 titanic.Age.describe()
 # The mean and the median are close to each other with 29.9 and 28 respectively
 
+# First solution: Replacing missing Age value by the median depending on the title
+# Second solution: predicting missing Age with a random Forest
+# Third solution; predicting missing Age with a SVM
+
+titanic["Age_Randomforest"] = titanic.Age.copy() #Missing Age predicted by random forest
+titanic["Age_SVM"] = titanic.Age.copy() #Missing Age predicted by SVM
+titanic["Age_replace"] = titanic.Age.copy() #Missing Age replaced by median depending on title
+
+# We will keep a variable Age for each solution and see later which one is better
+
+# //-- Replacing Age depending on title \\-- #
+
+Age_by_title = pd.DataFrame({'mean_age': titanic.groupby('Title').mean().loc[:, "Age"],
+                             'median_age': titanic.groupby('Title').median().loc[:, "Age"],
+                             'count': titanic.Title.value_counts(),
+                             'age_missing': titanic.Age.isnull().groupby(titanic['Title']).sum()})
+# We now have the mean and the median age for each title as well as the number of missing age for each title
+
+# We will now replace the missing age by the median age depending on the title
+for index, i in enumerate(titanic["Age_replace"]):
+    if pd.isnull(i) == False:
+        titanic.loc[index, "Age_replace"] = i
+    else:
+        i_title = titanic.loc[index, "Title"]
+        titanic.loc[index, 'Age_replace'] = Age_by_title.loc[i_title, "median_age"]
+
+titanic=titanic.drop('Title', axis=1)
+
+# //-- Random forest for Age \\-- #
+
 # we will now split the dataset into 2 datasets, the one with no empty age will be used as training data for the model
-titanic_WithAge = titanic[pd.isnull(titanic['Age']) == False]
-titanic_WithoutAge = titanic[pd.isnull(titanic['Age'])]
+titanic_WithAge = titanic[pd.isnull(titanic['Age_Randomforest']) == False]
+titanic_WithoutAge = titanic[pd.isnull(titanic['Age_Randomforest'])]
 
 # We will use theses variables as independent variables to predict the Age
 independent_variables = ['Pclass_1', 'Pclass_2', 'Pclass_3', 'female', 'male', 'C', 'Q', 'S',
                          'Dr', 'Master', 'Miss', 'Mr', 'Mrs', 'Nobility', 'Officer',
                          'big_family', 'small_family', 'solo', 'Parch', "SibSp", 'Fare']
 
-
-# //-- Features selections by Lasso Regression \\-- #
-#
-# x_train = titanic_WithAge.loc[:, independent_variables]
-# y_train = titanic_WithAge.loc[:, 'Age']
-#
-# Model_Lasso = LassoLarsCV(cv = 10, precompute = False).fit(x_train, y_train)
-#
-# lasso_coefs = pd.DataFrame(Model_Lasso.coef_)
-# tlasso_coefs['features'] = pd.Series(titanic_WithAge.loc[:, independent_variables].columns)
-# tlasso_coefs = tlasso_coefs.sort_values(by=[0])
-#
-# lasso_coef = list(tlasso_coefs.loc[tlasso_coefs[0] > 0, "features"])
-
-
-# //-- First Random forest estimator \\-- #
-
 rfModel_Age = RandomForestRegressor()
 
 age_accuracies = cross_val_score(estimator=rfModel_Age,
                                  X=titanic_WithAge.loc[:, independent_variables],
-                                 y=titanic_WithAge.loc[:, 'Age'],
+                                 y=titanic_WithAge.loc[:, 'Age_Randomforest'],
                                  cv=10,
                                  n_jobs=2)
 
@@ -181,79 +192,101 @@ print("The MEAN CV score is", round(age_accuracies.mean(), ndigits=2))
 print("The standard deviation is", round(age_accuracies.std(), ndigits=2))
 
 # The MEAN CV score is 0.36
-# The standard deviation is 0.09
+# The standard deviation is 0.08
 
-# //--  Hyperparameter tuning for Age Random forest  \\-- #
+# # //--  Hyperparameter tuning for Age Random forest  \\-- #
+#
+# # Number of trees in random forest
+# n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+# # Number of features to consider at every split
+# max_features = ['auto', 'sqrt']
+# # Maximum number of levels in tree
+# max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+# max_depth.append(None)
+# # Minimum number of samples required to split a node
+# min_samples_split = [2, 5, 10]
+# # Minimum number of samples required at each leaf node
+# min_samples_leaf = [1, 2, 4]
+# # Method of selecting samples for training each tree
+# bootstrap = [True, False]
+#
+# random_grid = {'n_estimators': n_estimators,
+#                'max_features': max_features,
+#                'max_depth': max_depth,
+#                'min_samples_split': min_samples_split,
+#                'min_samples_leaf': min_samples_leaf,
+#                'bootstrap': bootstrap}
+#
+# rf_random = RandomizedSearchCV(estimator = rfModel_Age,
+#                                param_distributions = random_grid,
+#                                n_iter = 100,
+#                                cv = 10,
+#                                verbose=2,
+#                                random_state=1234,
+#                                n_jobs = 2)
+#
+# rf_random.fit(titanic_WithAge.loc[:, independent_variables], titanic_WithAge.loc[:, 'Age'])
+#
+# rf_random.best_params_
+#
+# Tunned_rfModel_Age = RandomForestRegressor(n_estimators = 2000,
+#                                            min_samples_split=5,
+#                                            min_samples_leaf=4,
+#                                            max_features="sqrt",
+#                                            max_depth=10,
+#                                            bootstrap=False,
+#                                            random_state=1234)
+#
+# Tunned_age_accuracies = cross_val_score(estimator=Tunned_rfModel_Age,
+#                                         X=titanic_WithAge.loc[:, independent_variables],
+#                                         y=titanic_WithAge.loc[:, 'Age'],
+#                                         cv=10,
+#                                         n_jobs=2)
+#
+# print("The new MEAN CV score is", round(Tunned_age_accuracies.mean(), ndigits=2))
+# print("The new standard deviation is", round(Tunned_age_accuracies.std(), ndigits=2))
+#
+# # The MEAN CV score is 0.45
+# # The standard deviation is 0.06
+# # The model has increase by 8% and the standard deviation has decreased by 3%
 
-# Number of trees in random forest
-n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
-# Number of features to consider at every split
-max_features = ['auto', 'sqrt']
-# Maximum number of levels in tree
-max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
-max_depth.append(None)
-# Minimum number of samples required to split a node
-min_samples_split = [2, 5, 10]
-# Minimum number of samples required at each leaf node
-min_samples_leaf = [1, 2, 4]
-# Method of selecting samples for training each tree
-bootstrap = [True, False]
+# Fit the tunned model in the dataset
 
-random_grid = {'n_estimators': n_estimators,
-               'max_features': max_features,
-               'max_depth': max_depth,
-               'min_samples_split': min_samples_split,
-               'min_samples_leaf': min_samples_leaf,
-               'bootstrap': bootstrap}
+rfModel_Age.fit(titanic_WithAge.loc[:, independent_variables], titanic_WithAge.loc[:, 'Age_Randomforest'])
 
-rf_random = RandomizedSearchCV(estimator = rfModel_Age,
-                               param_distributions = random_grid,
-                               n_iter = 100,
-                               cv = 10,
-                               verbose=2,
-                               random_state=1234,
-                               n_jobs = 2)
-
-rf_random.fit(titanic_WithAge.loc[:, independent_variables], titanic_WithAge.loc[:, 'Age'])
-
-rf_random.best_params_
-
-Tunned_rfModel_Age = RandomForestRegressor(n_estimators = 2000,
-                                           min_samples_split=5,
-                                           min_samples_leaf=4,
-                                           max_features="sqrt",
-                                           max_depth=10,
-                                           bootstrap=False,
-                                           random_state=1234)
-
-Tunned_age_accuracies = cross_val_score(estimator=Tunned_rfModel_Age,
-                                        X=titanic_WithAge.loc[:, independent_variables],
-                                        y=titanic_WithAge.loc[:, 'Age'],
-                                        cv=10,
-                                        n_jobs=2)
-
-print("The new MEAN CV score is", round(Tunned_age_accuracies.mean(), ndigits=2))
-print("The new standard deviation is", round(Tunned_age_accuracies.std(), ndigits=2))
-
-# The MEAN CV score is 0.45
-# The standard deviation is 0.06
-# The model has increase by 8% and the standard deviation has decreased by 3%
-
-# //--  Fit the tunned model in the dataset  \\-- #
-
-Tunned_rfModel_Age.fit(titanic_WithAge.loc[:, independent_variables], titanic_WithAge.loc[:, 'Age'])
-
-titanic_WithoutAge.loc[:, 'Age'] = Tunned_rfModel_Age.predict(X = titanic_WithoutAge.loc[:, independent_variables]).astype(int)
+titanic_WithoutAge.loc[:, 'Age_Randomforest'] = rfModel_Age.predict(X = titanic_WithoutAge.loc[:, independent_variables]).astype(int)
 
 titanic = titanic_WithAge.append(titanic_WithoutAge).sort_values(by=['PassengerId']).reset_index(drop=True)
 
-# We now have all observations of Age without missing values by random forest prediction
+# //-- SVM for Age \\-- #
 
-titanic.Age.describe()
 plt.hist(titanic.Age)
 plt.hist(titanic.Age, range=(0, 30))
 plt.hist(titanic.Age, range=(30, 80))
 # the distribution of the age looks almost like a normal distribution
+
+titanic_WithAge_SVM = titanic[pd.isnull(titanic['Age']) == False]
+titanic_WithoutAge_SVM = titanic[pd.isnull(titanic['Age'])]
+
+SVM_reg = SVR(kernel="linear")
+
+age_accuracies_SVM = cross_val_score(estimator=SVM_reg,
+                                 X=titanic_WithAge_SVM.loc[:, independent_variables],
+                                 y=titanic_WithAge_SVM.loc[:, 'Age_SVM'],
+                                 cv=10,
+                                 n_jobs=2)
+
+print("The MEAN CV score is", round(age_accuracies_SVM.mean(), ndigits=2))
+print("The standard deviation is", round(age_accuracies_SVM.std(), ndigits=2))
+
+# The MEAN CV score is 0.39
+# The standard deviation is 0.06
+
+SVM_reg.fit(titanic_WithAge_SVM.loc[:, independent_variables], titanic_WithAge_SVM.loc[:, 'Age_SVM'])
+
+titanic_WithoutAge_SVM.loc[:, 'Age_SVM'] = SVM_reg.predict(X = titanic_WithoutAge_SVM.loc[:, independent_variables]).astype(int)
+
+titanic = titanic_WithAge_SVM.append(titanic_WithoutAge_SVM).sort_values(by=['PassengerId']).reset_index(drop=True)
 
 # ==================== SEPARATE THE DATA AGAIN AND GET BACK OUR TRAIN/TEST DATASETS ==================== #
 
